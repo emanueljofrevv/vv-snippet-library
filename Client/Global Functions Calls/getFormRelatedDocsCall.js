fe3ba6ebe66db2bb5ca6bb660fc6f237f093fe7f
fernandoCulell <fernando.culell@onetree.com>
2022-06-29 16:51:26 -0300

    Add getFormRelatedDocsCall

---
+++ Client/Global Functions Calls/getFormRelatedDocsCall.js
@@ -0,0 +1,10 @@
+// Get the related Documents
+// RevisionID = GUID of the form
+// shortDescription is a required global variable
+
+const relatedDocumentsResp = await vvClient.forms
+    .getFormRelatedDocs(RevisionID, null)
+    .then((res) => parseRes(res))
+    .then((res) => checkMetaAndStatus(res, shortDescription))
+    .then((res) => checkDataPropertyExists(res, shortDescription))
+    .then((res) => checkDataIsNotEmpty(res, shortDescription));
